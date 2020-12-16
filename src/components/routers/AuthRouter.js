e7f2a4b0d2c8f015ac61d30cb18b781dbed7fb89
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-12-15 20:25:28 -0600

    Se crearon los archivos de sass y se trabajo en el login screen

--- src/components/routers/AuthRouter.js
+++ src/components/routers/AuthRouter.js
@@ -5,20 +5,22 @@ import {RegisterScreen} from "../auth/RegisterScreen";
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route
-                    exact
-                    path="/auth/login"
-                    component={LoginScreen}
-                />
-                <Route
-                    exact
-                    path="/auth/register"
-                    component={RegisterScreen}
-                />
-                <Redirect to="/auth/register" />
-            </Switch>
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route
+                        exact
+                        path="/auth/login"
+                        component={LoginScreen}
+                    />
+                    <Route
+                        exact
+                        path="/auth/register"
+                        component={RegisterScreen}
+                    />
+                    <Redirect to="/auth/register" />
+                </Switch>
+            </div>
         </div>
     );
 };
